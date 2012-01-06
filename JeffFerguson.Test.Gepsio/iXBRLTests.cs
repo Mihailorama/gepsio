d4e50c318e5e03595d95c93f39ab483251045faf
JeffreyFerguson_cp <gepsio@outlook.com>
2012-01-06 19:22:37 +0000

    Added first iXBRL test (which will fail at the moment, since Gepsio does not yet support iXBRL markup).

---
+++ JeffFerguson.Test.Gepsio/iXBRLTests.cs
@@ -0,0 +1,18 @@
+﻿using JeffFerguson.Gepsio;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JeffFerguson.Test.Gepsio
+{
+	[TestClass]
+	public class iXBRLTests
+	{
+		[TestMethod]
+		public void UKGAAP2009Test()
+		{
+			var doc = new XbrlDocument();
+			doc.Load(@"..\..\..\JeffFerguson.Test.Gepsio\ixbrl-samples\UKGAAP2009.xml");
+			Assert.IsNotNull(doc.XbrlFragments);
+			Assert.AreNotEqual<int>(0, doc.XbrlFragments.Count, "No fragments found in document.");
+		}
+	}
+}
