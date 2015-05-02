fdfc8562facb0b9415bfae4fc8aa1088684e7561
JeffreyFerguson_cp <gepsio@outlook.com>
2015-05-02 22:42:47 +0000

    Updated version number to 2.1.0.9. Changes made to support help file generation for version 2.1.0.9.

--- JeffFerguson.Gepsio/Properties/AssemblyInfo.cs
+++ JeffFerguson.Gepsio/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jeff Ferguson")]
 [assembly: AssemblyProduct("Gepsio")]
-[assembly: AssemblyCopyright("Copyright © Jeff Ferguson 2009-2014")]
+[assembly: AssemblyCopyright("Copyright © Jeff Ferguson 2009-2015")]
 [assembly: AssemblyTrademark("http://gepsio.codeplex.com")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0.8")]
-[assembly: AssemblyFileVersion("2.1.0.8")]
+[assembly: AssemblyVersion("2.1.0.9")]
+[assembly: AssemblyFileVersion("2.1.0.9")]
