baa28ed3c2dcf4f2fe8faf43d4e42eae7501a237
JeffreyFerguson_cp <gepsio@outlook.com>
2015-07-18 16:31:15 +0000

    Added a unit test to ensure that a presentable fact tree can be walked without crashing.

--- JeffFerguson.Test.Gepsio/PresentableFactTreeTests.cs
+++ JeffFerguson.Test.Gepsio/PresentableFactTreeTests.cs
@@ -1,5 +1,6 @@
 ﻿using JeffFerguson.Gepsio;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
 using System.Text;
 
 namespace JeffFerguson.Test.Gepsio
@@ -18,6 +19,38 @@ namespace JeffFerguson.Test.Gepsio
             var firstFragment = xbrlDoc.XbrlFragments[0];
             var tree = firstFragment.GetPresentableFactTree();
             Assert.IsNotNull(tree);
+        }
+
+        [TestMethod]
+        public void WalkPresentableTree()
+        {
+            var xbrlDoc = new XbrlDocument();
+            xbrlDoc.Load(GetRelativePathForDocument("aig-20130630.xml"));
+            var firstFragment = xbrlDoc.XbrlFragments[0];
+            var tree = firstFragment.GetPresentableFactTree();
+            foreach (var currentNode in tree.TopLevelNodes)
+                WalkPresentableTreeNode(currentNode, 0);
+        }
+
+        private void WalkPresentableTreeNode(PresentableFactTreeNode node, int depth)
+        {
+            for (var indent = 0; indent < depth; indent++)
+                Debug.Write("    ");
+            if (node.NodeFact != null)
+            {
+                if (node.NodeFact is Item)
+                {
+                    var nodeItem = node.NodeFact as Item;
+                    Debug.Write(nodeItem.Name);
+                    Debug.Write(" ");
+                    Debug.Write(nodeItem.Value);
+                }
+            }
+            else
+                Debug.Write(node.NodeLabel);
+            Debug.WriteLine("");
+            foreach (var childNode in node.ChildNodes)
+                WalkPresentableTreeNode(childNode, depth + 1);
         }
 
         private string GetRelativePathForDocument(string DocFilename)
