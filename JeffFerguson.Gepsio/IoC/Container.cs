d80b494ac5828d56c9126ad4876d4c66b09bcc35
JeffreyFerguson_cp <gepsio@outlook.com>
2015-08-26 00:37:29 +0000

    XML interface implementations are no longer hardcoded during IoC container construction and are now discovered at runtime through reflection.

--- JeffFerguson.Gepsio/IoC/Container.cs
+++ JeffFerguson.Gepsio/IoC/Container.cs
@@ -1,6 +1,7 @@
 ﻿using JeffFerguson.Gepsio.Xml.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace JeffFerguson.Gepsio.IoC
 {
@@ -10,39 +11,87 @@ namespace JeffFerguson.Gepsio.IoC
     internal static class Container
     {
         private static Dictionary<Type, Type> registeredTypes;
+        private static Assembly currentAssembly;
+        private static Type[] allTypes;
 
         static Container()
         {
             registeredTypes = new Dictionary<Type, Type>();
-            RegisterSystemXmlTypes();
+            currentAssembly = Assembly.GetExecutingAssembly();
+            allTypes = currentAssembly.GetTypes();
+            RegisterAllTypes();
         }
 
-        private static void RegisterSystemXmlTypes()
+        private static void RegisterAllTypes()
         {
-            Register<IAttribute, JeffFerguson.Gepsio.Xml.Implementation.SystemXml.Attribute>();
-            Register<IAttributeList, JeffFerguson.Gepsio.Xml.Implementation.SystemXml.AttributeList>();
-            Register<IDocument, JeffFerguson.Gepsio.Xml.Implementation.SystemXml.Document>();
-            Register<INamespaceManager, JeffFerguson.Gepsio.Xml.Implementation.SystemXml.NamespaceManager>();
-            Register<INode, JeffFerguson.Gepsio.Xml.Implementation.SystemXml.Node>();
-            Register<INodeList, JeffFerguson.Gepsio.Xml.Implementation.SystemXml.NodeList>();
-            Register<IQualifiedName, JeffFerguson.Gepsio.Xml.Implementation.SystemXml.QualifiedName>();
-            Register<ISchema, JeffFerguson.Gepsio.Xml.Implementation.SystemXml.Schema>();
-            Register<ISchemaElement, JeffFerguson.Gepsio.Xml.Implementation.SystemXml.SchemaElement>();
-            Register<ISchemaSet, JeffFerguson.Gepsio.Xml.Implementation.SystemXml.SchemaSet>();
-            Register<ISchemaType, JeffFerguson.Gepsio.Xml.Implementation.SystemXml.SchemaType>();
+            Register<IAttribute>();
+            Register<IAttributeList>();
+            Register<IDocument>();
+            Register<INamespaceManager>();
+            Register<INode>();
+            Register<INodeList>();
+            Register<IQualifiedName>();
+            Register<ISchema>();
+            Register<ISchemaElement>();
+            Register<ISchemaSet>();
+            Register<ISchemaType>();
         }
 
-        private static void Register<TInterface, TImplementation>()
-        {
-            registeredTypes.Add(typeof(TInterface), typeof(TImplementation));
-        }
-
-        public static TInterface Resolve<TInterface>()
+        /// <summary>
+        /// Resolves an interface to a created type.
+        /// </summary>
+        /// <typeparam name="TInterface">
+        /// The interface to be resolved.
+        /// </typeparam>
+        /// <returns>
+        /// An object of the type that implements the interface.
+        /// </returns>
+        internal static TInterface Resolve<TInterface>()
         {
             if (registeredTypes.ContainsKey(typeof(TInterface)) == false)
                 throw new KeyNotFoundException("Interface type not registered.");
             Type implementationType = registeredTypes[typeof(TInterface)];
             return (TInterface)Activator.CreateInstance(implementationType);
+        }
+
+        /// <summary>
+        /// Registers an interface with the container.
+        /// </summary>
+        /// <remarks>
+        /// Only the interface need be specified. The methods automatically finds the
+        /// class that implements the interface and associates the class' type in the
+        /// container with the interface.
+        /// </remarks>
+        /// <typeparam name="TInterface">
+        /// The interface to be registered.
+        /// </typeparam>
+        private static void Register<TInterface>()
+        {
+            var implementationType = FindTypeWithInterfaceImplementation<TInterface>();
+            registeredTypes.Add(typeof(TInterface), implementationType);
+        }
+
+        /// <summary>
+        /// Finds the class that implements the given interface and returns its type.
+        /// </summary>
+        /// <typeparam name="TInterface">
+        /// The interface whose implementation should be found.
+        /// </typeparam>
+        /// <returns>
+        /// The type of class that implements the given interface.
+        /// </returns>
+        private static Type FindTypeWithInterfaceImplementation<TInterface>()
+        {
+            foreach (var currentType in allTypes)
+            {
+                var typeInterfaces = currentType.GetInterfaces();
+                foreach (var currentInterface in typeInterfaces)
+                {
+                    if (currentInterface.Equals(typeof(TInterface)) == true)
+                        return currentType;
+                }
+            }
+            throw new TypeLoadException();
         }
     }
 }
